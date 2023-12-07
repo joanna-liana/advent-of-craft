97ae5ace133495993a4be86f1eaa077d140b1a73
Joanna Liana <joanna_liana@outlook.com>
2023-12-07 08:09:37 +0100

    Day 2 solution - add refactored implementation

---
+++ solution/day02/src/CHANGELOG.md
@@ -0,0 +1,4 @@
+Implementation:
+- checked the invalid input range at the beginning of the method
+- simplified the min threshold check, assuming the min accepted would be 1
+- removed the redundant range checks from the remaining code, which nicely led to just one level of indentation
--- solution/day02/src/FizzBuzz.ts
+++ solution/day02/src/FizzBuzz.ts
@@ -4,23 +4,22 @@ export class FizzBuzz {
   private constructor() {}
 
   public static convert(input: number): string {
-    if (input > 0) {
-      if (input <= 100) {
-        if (input % 3 === 0 && input % 5 === 0) {
-          return 'FizzBuzz';
-        }
-        if (input % 3 === 0) {
-          return 'Fizz';
-        }
-        if (input % 5 === 0) {
-          return 'Buzz';
-        }
-        return input.toString();
-      } else {
-        throw new OutOfRangeException();
-      }
-    } else {
+    if (input < 1 || input > 100) {
       throw new OutOfRangeException();
     }
+
+    if (input % 3 === 0 && input % 5 === 0) {
+      return 'FizzBuzz';
+    }
+
+    if (input % 3 === 0) {
+      return 'Fizz';
+    }
+
+    if (input % 5 === 0) {
+      return 'Buzz';
+    }
+
+    return input.toString();
   }
 }
