3300a2310d88d3339f8a3c54c50fd6271eff2b83
Joanna Liana <joanna_liana@outlook.com>
2023-12-10 13:30:36 +0100

    Day 3 solution - clean up tests

--- solution/day03/src/CHANGELOG.md
+++ solution/day03/src/CHANGELOG.md
@@ -1,8 +1,9 @@
 Implementation:
 - Change `Person.addPet` signature to accept the `Pet` to add - the method is no longer concerned with constructing the pet
-- Improve `Person.pets`` encaspsulation (no longer mutable from outside)
+- Improve `Person.pets` encaspsulation (no longer mutable from outside)
 
 Tests:
 - Improve readability:
   - Divide the test body into more meaningful variables
   - Extract a helper for extracting the youngest pet age by person
+  - Assert the actual person, not just the name (inspired by [Yann's solution](https://github.com/yanncourtel/advent-of-craft-2023/blob/f2f732b1675582f38f62d6e7b24a1f56538d4273/exercise/day03/src/test/java/PopulationTests.java#L30))
--- solution/day03/src/test/population.test.ts
+++ solution/day03/src/test/population.test.ts
@@ -2,44 +2,51 @@ import { Person } from '../Person';
 import { Pet } from '../Pet';
 import { PetType } from '../PetType';
 
-// TODO: is the concept of population missing?
-let population: Person[];
+describe('Population', () => {
+  let population: Person[];
+  let expectedYoungestPetOwner;
 
-beforeAll(() => {
-  population = [
-    new Person('Peter', 'Griffin').addPet(new Pet(PetType.CAT, 'Tabby', 2)),
-    new Person('Stewie', 'Griffin')
-      .addPet(new Pet(PetType.CAT, 'Dolly', 3))
-      .addPet(new Pet(PetType.DOG, 'Brian', 9)),
-    new Person('Joe', 'Swanson').addPet(new Pet(PetType.DOG, 'Spike', 4)),
-    new Person('Lois', 'Griffin').addPet(new Pet(PetType.SNAKE, 'Serpy', 1)),
-    new Person('Meg', 'Griffin').addPet(new Pet(PetType.BIRD, 'Tweety', 1)),
-    new Person('Chris', 'Griffin').addPet(new Pet(PetType.TURTLE, 'Speedy', 4)),
-    new Person('Cleveland', 'Brown')
-      .addPet(new Pet(PetType.HAMSTER, 'Fuzzy', 1))
-      .addPet(new Pet(PetType.HAMSTER, 'Wuzzy', 2)),
-    new Person('Glenn', 'Quagmire')
-  ];
-});
+  beforeEach(() => {
+    expectedYoungestPetOwner = new Person('Lois', 'Griffin')
+      .addPet(new Pet(PetType.SNAKE, 'Serpy', 1));
 
-const findYoungestPetAge = (person: Person) => {
-  const petsByAge = [...person.pets].sort((petA, petB) => petA.age - petB.age);
-  const youngestPet = petsByAge[0];
-
-  return youngestPet?.age;
-};
-
-
-test('who owns the youngest pet', () => {
-  const populationByYoungestPet = population.sort((personA, personB) => {
-    const aPetAge = findYoungestPetAge(personA);
-    const bPetAge = findYoungestPetAge(personB);
-
-    return aPetAge - bPetAge;
+    population = [
+      new Person('Peter', 'Griffin').addPet(new Pet(PetType.CAT, 'Tabby', 2)),
+      new Person('Stewie', 'Griffin')
+        .addPet(new Pet(PetType.CAT, 'Dolly', 3))
+        .addPet(new Pet(PetType.DOG, 'Brian', 9)),
+      new Person('Joe', 'Swanson').addPet(new Pet(PetType.DOG, 'Spike', 4)),
+      expectedYoungestPetOwner,
+      new Person('Meg', 'Griffin').addPet(new Pet(PetType.BIRD, 'Tweety', 1)),
+      new Person('Chris', 'Griffin')
+        .addPet(new Pet(PetType.TURTLE, 'Speedy', 4)),
+      new Person('Cleveland', 'Brown')
+        .addPet(new Pet(PetType.HAMSTER, 'Fuzzy', 1))
+        .addPet(new Pet(PetType.HAMSTER, 'Wuzzy', 2)),
+      new Person('Glenn', 'Quagmire')
+    ];
   });
 
-  const youngestPetOwner = populationByYoungestPet[0];
+  const findYoungestPetAge = (person: Person) => {
+    const petsByAge = [...person.pets]
+      .sort((petA, petB) => petA.age - petB.age);
+    const youngestPet = petsByAge[0];
 
-  expect(youngestPetOwner).toBeDefined();
-  expect(youngestPetOwner.firstName).toBe('Lois');
+    return youngestPet?.age;
+  };
+
+
+  test('who owns the youngest pet', () => {
+    const populationByYoungestPet = population.sort((personA, personB) => {
+      const aPetAge = findYoungestPetAge(personA);
+      const bPetAge = findYoungestPetAge(personB);
+
+      return aPetAge - bPetAge;
+    });
+
+    const youngestPetOwner = populationByYoungestPet[0];
+
+    expect(youngestPetOwner).toBe(youngestPetOwner);
+  });
+
 });
