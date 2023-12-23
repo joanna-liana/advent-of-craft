6886af5d37d0a0b825859be56782ac0594e8b10a
Joanna Liana <joanna_liana@outlook.com>
2023-12-23 14:55:19 +0100

    Day 4 solution - date tests' improvements

--- solution/day04/src/CHANGELOG.md
+++ solution/day04/src/CHANGELOG.md
@@ -1,3 +1,4 @@
 Improvements to the tests:
 1. "it should add valid comment" - no assertion; added the missing assertion by combining this and the following two tests: "it should add a comment with the given text", "it should add a comment with the given author"
 2. "it should add a comment with the date of the day" - no assertion, added the missing one.
+3. Added a test that checks if the provided comment date is saved.
--- solution/day04/src/test/article.test.ts
+++ solution/day04/src/test/article.test.ts
@@ -46,6 +46,25 @@ describe('Article', () => {
     );
   });
 
+  test('it should add a comment with the provided date', () => {
+    const article = new Article(
+      'Lorem Ipsum',
+      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+    );
+    const now = new Date(2020, 0, 12);
+    jest.setSystemTime(now);
+    const creationDate = new Date(2022, 5, 20);
+
+    article.addComment('Amazing article !!!', 'Pablo Escobar', creationDate);
+
+    expect(article.getComments()).toHaveLength(1);
+    expect(article.getComments()).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ creationDate })
+      ])
+    );
+  });
+
   test('it should throw an exception when adding existing comment', () => {
     const article = new Article(
       'Lorem Ipsum',
