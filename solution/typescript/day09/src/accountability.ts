33dd64ed2f08564234f3bd0b346f9fc9bc9645d7
Joanna Liana <joanna_liana@outlook.com>
2024-10-19 13:10:04 +0200

    Day 9 - fix bug, splitting queries and commands

--- solution/typescript/day09/src/accountability.ts
+++ solution/typescript/day09/src/accountability.ts
@@ -5,17 +5,36 @@ export class Client {
   }
 
   toStatement(): string {
-    const lines = Object.entries(this.orderLines).map(([name, value]) =>
-      this.formatLine(name, value));
+    this.calculateTotal();
+
+    return this.generateStatement();
+  }
+
+  private generateStatement(): string {
+    const lines = this.generateItemLines();
+
     return `${lines.join('\n')}\nTotal : ${this.totalAmount.toFixed(2)}€`;
+  }
+
+  private generateItemLines(): string[] {
+    return Object.entries(this.orderLines)
+      .map(([name, value]) => this.formatLine(name, value));
   }
 
   getTotalAmount(): number {
     return this.totalAmount;
   }
 
+  private calculateTotal(): void {
+    this.totalAmount = Object.entries(this.orderLines)
+      .reduce((total, [, value]) => {
+        total += value;
+
+        return total;
+      }, 0);
+  }
+
   private formatLine(name: string, value: number): string {
-    this.totalAmount += value;
     return `${name} for ${value.toFixed(2)}€`;
   }
 }
