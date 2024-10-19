a1cd3ae2f98b079ac1cba0511730694339292971
Joanna Liana <joanna_liana@outlook.com>
2024-10-19 13:04:15 +0200

    Day 9 - add tests showing bug

---
+++ solution/typescript/day09/tests/accountability.spec.ts
@@ -0,0 +1,40 @@
+import {
+  Client
+} from '../src/accountability'; // Adjust the import path as needed
+
+describe('Client Tests', () => {
+  const client = new Client({
+    'Tenet Deluxe Edition': 45.99,
+    'Inception': 30.50,
+    'The Dark Knight': 30.50,
+    'Interstellar': 23.98
+  });
+
+  test('returns statement on first call', () => {
+    let statement = client.toStatement();
+    statement = client.toStatement();
+
+    expect(client.getTotalAmount()).toBeCloseTo(130.97, 2);
+    expect(statement.trim()).toEqual(
+      'Tenet Deluxe Edition for 45.99€\n' +
+            'Inception for 30.50€\n' +
+            'The Dark Knight for 30.50€\n' +
+            'Interstellar for 23.98€\n' +
+            'Total : 130.97€'
+    );
+  });
+
+  test('returns correct statemement when called again', () => {
+    let statement = client.toStatement();
+    statement = client.toStatement();
+
+    expect(client.getTotalAmount()).toBeCloseTo(130.97, 2);
+    expect(statement.trim()).toEqual(
+      'Tenet Deluxe Edition for 45.99€\n' +
+            'Inception for 30.50€\n' +
+            'The Dark Knight for 30.50€\n' +
+            'Interstellar for 23.98€\n' +
+            'Total : 130.97€'
+    );
+  });
+});
