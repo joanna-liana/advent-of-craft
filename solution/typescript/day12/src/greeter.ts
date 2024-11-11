54cbd57f3a46e8ca2d639205d1cdb83f219c6adf
Joanna Liana <joanna_liana@outlook.com>
2024-11-11 16:55:11 +0100

    Day 12 - Support configurable formality

--- solution/typescript/day12/src/greeter.ts
+++ solution/typescript/day12/src/greeter.ts
@@ -1,9 +1,6 @@
-interface GreetingVariants {
+type GreetingVariants =  {
     default: string;
-    formal: string;
-    casual: string;
-    intimate: string;
-}
+} & Record<string, string>
 
 export class Greeter {
   private formality: string | null = null;
@@ -18,23 +15,7 @@ export class Greeter {
   }
 
   greet(): string {
-    if (this.formality == null) {
-      return this.config.default;
-    }
-
-    if (this.formality === 'formal') {
-      return this.config.formal;
-    }
-
-    if (this.formality === 'casual') {
-      return this.config.casual;
-    }
-
-    if (this.formality === 'intimate') {
-      return this.config.intimate;
-    }
-
-    return this.config.default;
+    return this.config[this.formality] ?? this.config.default;
   }
 
   setFormality(formality: string): void {
